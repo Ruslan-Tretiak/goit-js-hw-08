e566588833f2bcd1559e6b639e158039164e2be8
Ruslan-Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-01 21:55:15 +0000

    Deploying to gh-pages from @ Ruslan-Tretiak/goit-js-hw-08@c3603a76715851b7df4bf803fa937eebcdbd8601 🚀

--- index.2744e830.js
+++
@@ -1,2 +0,0 @@
-console.log("HELLO MY BEAUTIFULL WORLD");
-//# sourceMappingURL=index.2744e830.js.map
--- index.54173d63.js
+++
@@ -1,2 +0,0 @@
-console.log("HELLO MY BEAUTIFULL WORLD");
-//# sourceMappingURL=index.54173d63.js.map
