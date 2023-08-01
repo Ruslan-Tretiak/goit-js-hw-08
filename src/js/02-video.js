88d501fb3a6948813d2039534bae23f434b87a00
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-02 00:13:56 +0100

    Task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,27 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from "lodash.throttle";
+
+const framePlayer = document.querySelector('iframe');
+const player = new Player(framePlayer);
+
+
+const onPlay = function({seconds}) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+const storage = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(storage).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the vnpmideo’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
