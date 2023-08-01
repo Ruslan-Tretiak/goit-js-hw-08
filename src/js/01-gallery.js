c3603a76715851b7df4bf803fa937eebcdbd8601
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-01 22:54:50 +0100

    Task #1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items';
+console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const gallery = document.querySelector('.gallery');
+const imgList = galleryItems.map(({ preview, original, description }) => `
+        <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" />
+   </a>
+</li>`).join(' ');
+
+gallery.insertAdjacentHTML('afterbegin', imgList);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250
+})
+
+
