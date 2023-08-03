eeafc907d66c49701594ddb3bed4ad3bb1f03860
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-03 23:54:32 +0100

    Task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.elements.email;
+const messageInput = feedbackForm.elements.message;
+
+
+const saveFeedback = () => {
+    const inputItems = {
+        email: emailInput.value,
+        massage: messageInput.value,
+    };
+localStorage.setItem("feedback-form-state", JSON.stringify(inputItems));
+}   
+const loadFeedback = () => {
+  const feedbackData = localStorage.getItem("feedback-form-state");
+  if (feedbackData) {
+    const inputItems = JSON.parse(feedbackData);
+    emailInput.value = inputItems.email;
+    messageInput.value = inputItems.message;
+  }
+};
+
+feedbackForm.addEventListener('input', saveFeedback);
+loadFeedback();
+
+
+
+
+
+
 
