ef3d7c0b4968b86abc00baca370a03ecc51e6a73
Ruslan Tretiak <99765530+Ruslan-Tretiak@users.noreply.github.com>
2023-08-06 23:53:09 +0100

    TASK#3 fix submit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,43 @@
 const feedbackForm = document.querySelector('.feedback-form');
 
 function saveFeedbackToLocalStorage() {
-    const formData = new FormData(feedbackForm);
-    const feedback = {};
-    formData.forEach((value, name) => {
-      feedback[name] = value;
-    });
+  const emailInput = feedbackForm.elements.email;
+  const messageInput = feedbackForm.elements.message;
+
+  const feedback = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+
+  try {
     const serializedFeedback = JSON.stringify(feedback);
-    localStorage.setItem('feedback-form-state', serializedFeedback); 
+    localStorage.setItem('feedback-form-state', serializedFeedback);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
 }
+
 function loadFeedbackFromLocalStorage() {
+  try {
     const serializedFeedback = localStorage.getItem('feedback-form-state');
     if (serializedFeedback) {
-        const feedback = JSON.parse(serializedFeedback);
-        for (const name in feedback) {
-            const input = feedbackForm.elements[name];
-            if (input) {
-                input.value = feedback[name];
-            }
-        }
+      const feedback = JSON.parse(serializedFeedback);
+      feedbackForm.elements.email.value = feedback.email;
+      feedbackForm.elements.message.value = feedback.message;
     }
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
 }
+
 function resetFeedbackForm() {
   feedbackForm.reset();
   saveFeedbackToLocalStorage();
 }
 
 feedbackForm.addEventListener('input', saveFeedbackToLocalStorage);
-feedbackForm.addEventListener('submit', function(evt) {
-  evt.preventDefault();
+feedbackForm.addEventListener('submit', function(event) {
+  event.preventDefault();
   resetFeedbackForm();
 });
 
